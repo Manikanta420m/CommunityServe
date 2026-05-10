e21d9e7dda85943639bf7b08de801987d708e22e
Manikanta420m <manikanta420m@gmail.com>
2026-05-10 17:20:15 +0530

    Implement issue reporting form using React state

--- CommunityServe/src/pages/reportIssues.jsx
+++ CommunityServe/src/pages/reportIssues.jsx
@@ -1,7 +1,62 @@
+import { useState } from "react";
+
 function ReportIssue() {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    location: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formData);
+  };
+
   return (
-    <div>
+    <div className="container">
       <h1>Report Issue</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Issue Title"
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <textarea
+          name="description"
+          placeholder="Description"
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="text"
+          name="location"
+          placeholder="Location"
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
