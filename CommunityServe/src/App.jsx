46f04b4a5f219170e1f75457eaeeb857a6982e02
Manikanta420m <manikanta420m@gmail.com>
2026-05-10 18:12:49 +0530

    "checking issues"

--- CommunityServe/src/App.jsx
+++ CommunityServe/src/App.jsx
@@ -1,20 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from "./pages/home";
-import ReportIssue from "./pages/reportIssue";
-import Navbar from "./components/navbar";
-
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-
-        <Route path="/report" element={<ReportIssue />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>Hello Mani 🚀</h1>
+      <p>Frontend is working successfully.</p>
+    </div>
   );
 }
 
