56afd1d8ba6eee44fcfd22490d179a9aca551534
Manikanta420m <manikanta420m@gmail.com>
2026-05-10 17:16:20 +0530

    creating some dummy issues for people

--- CommunityServe/src/pages/home.jsx
+++ CommunityServe/src/pages/home.jsx
@@ -1,7 +1,33 @@
+import IssueCard from "../components/issueCard";
+
 function Home() {
+  const issues = [
+    {
+      id: 1,
+      title: "Broken Road",
+      description: "Road damaged near bus stand",
+      location: "Nellore",
+      status: "Pending",
+      votes: 5,
+    },
+
+    {
+      id: 2,
+      title: "Garbage Overflow",
+      description: "Garbage not cleaned",
+      location: "Market Area",
+      status: "In Progress",
+      votes: 10,
+    },
+  ];
+
   return (
-    <div>
+    <div className="container">
       <h1>Community Issues</h1>
+
+      {issues.map((issue) => (
+        <IssueCard key={issue.id} issue={issue} />
+      ))}
     </div>
   );
 }
