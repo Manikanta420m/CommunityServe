96b9b040841a7b094a2c7d640dc40908de77044f
Manikanta420m <manikanta420m@gmail.com>
2026-05-12 17:08:44 +0530

    "Implement user authentication APIs with JWT"

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -15,8 +15,12 @@ app.get("/", (req, res) => {
   res.send("Backend Running Successfully");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const authRoutes = require("./routes/authRoutes");
+
+app.use("/api/auth", authRoutes);
