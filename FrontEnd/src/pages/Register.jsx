83a6908dd398f8cef56a222fbbfe08a78cab85da
Manikanta420m <manikanta420m@gmail.com>
2026-05-12 17:53:39 +0530

    "Created Register Page"

--- FrontEnd/src/pages/Register.jsx
+++ FrontEnd/src/pages/Register.jsx
@@ -1,7 +1,70 @@
+import { useState } from "react";
+import { registerUser } from "../services/authService";
+
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const data = await registerUser(formData);
+
+      console.log(data);
+
+      alert("User Registered Successfully");
+    } catch (error) {
+      console.log(error);
+
+      alert("Registration Failed");
+    }
+  };
+
   return (
     <div>
-      <h1>Register Page</h1>
+      <h1>Register</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter Name"
+          onChange={handleChange}
+        />
+
+        <br />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter Email"
+          onChange={handleChange}
+        />
+
+        <br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Password"
+          onChange={handleChange}
+        />
+
+        <br />
+
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 };
