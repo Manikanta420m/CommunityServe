4b321568d1c4254fba59dd27b06a8937a17b3705
Manikanta420m <manikanta420m@gmail.com>
2026-05-12 17:46:16 +0530

    "createissues and App.jsx update"

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import CreateIssue from "./pages/CreateIssue";
+
 function App() {
   return (
-    <div>
-      <h1>CommunityServe</h1>
-      <p>Frontend setup completed successfully 🚀</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/create-issue" element={<CreateIssue />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ FrontEnd/src/pages/CreateIssue.jsx
@@ -0,0 +1,9 @@
+const CreateIssue = () => {
+  return (
+    <div>
+      <h1>Create Issue</h1>
+    </div>
+  );
+};
+
+export default CreateIssue;
