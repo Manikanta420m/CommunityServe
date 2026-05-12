16ebfd031f985fdd8999a430865f6df5b207d0a2
Manikanta420m <manikanta420m@gmail.com>
2026-05-12 17:55:03 +0530

    "Login file update"

--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -1,7 +1,63 @@
+import { useState } from "react";
+
+import { loginUser } from "../services/authService";
+
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const data = await loginUser(formData);
+
+      console.log(data);
+
+      localStorage.setItem("token", data.token);
+
+      alert("Login Successful");
+    } catch (error) {
+      console.log(error);
+
+      alert("Login Failed");
+    }
+  };
+
   return (
     <div>
-      <h1>Login Page</h1>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter Email"
+          onChange={handleChange}
+        />
+
+        <br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Password"
+          onChange={handleChange}
+        />
+
+        <br />
+
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 };
