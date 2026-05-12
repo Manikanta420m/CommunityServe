6472d880a403f22ca51d5f757f6054bb3de53d85
Manikanta420m <manikanta420m@gmail.com>
2026-05-12 17:25:15 +0530

    "created Issues.js"

---
+++ BackEnd/models/Issue.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const issueSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    category: {
+      type: String,
+      required: true,
+    },
+
+    location: {
+      type: String,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      default: "Pending",
+    },
+
+    votes: {
+      type: Number,
+      default: 0,
+    },
+
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Issue =
+  mongoose.models.Issue || mongoose.model("Issue", issueSchema);
+
+module.exports = Issue;
