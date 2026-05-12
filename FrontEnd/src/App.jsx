86ae42c94406a47bc6aaa0fdff3c7c755f43c215
Manikanta420m <manikanta420m@gmail.com>
2026-05-12 18:26:09 +0530

    "toast management"

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/navbar";
 
+
+import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
@@ -13,13 +15,26 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
         <Route path="/login" element={<Login />} />
 
         <Route path="/register" element={<Register />} />
 
-        <Route path="/create-issue" element={<CreateIssue />} />
+     <Route
+        path="/create-issue"
+        element={
+          <ProtectedRoute>
+            <CreateIssue />
+          </ProtectedRoute>
+        }
+      />
       </Routes>
     </BrowserRouter>
   );
