41e2f7449d572756701e93d69f62e007767ee998
Manikanta420m <manikanta420m@gmail.com>
2026-05-12 17:16:13 +0530

    "changes in server and user"

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 
 const connectDB = require("./config/db");
 
+const authRoutes = require("./routes/authRoutes");
+const testRoutes = require("./routes/testRoutes");
+
 const app = express();
 
 connectDB();
@@ -11,16 +14,15 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/test", testRoutes);
+
 app.get("/", (req, res) => {
   res.send("Backend Running Successfully");
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-const authRoutes = require("./routes/authRoutes");
-
-app.use("/api/auth", authRoutes);
