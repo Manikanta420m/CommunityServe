6200e8e783df9da49df24fc8b07a86a81e48c871
ManiKanta <manikanta420m@gmail.com>
2026-09-02 16:53:00 +0530

    feat: expose admin user management routes

--- BackEnd/routes/userRoutes.js
+++ BackEnd/routes/userRoutes.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const { getAssignableUsers } = require("../controllers/userController");
+const {
+  getAssignableUsers,
+  getUsers,
+  updateUser,
+} = require("../controllers/userController");
 const { protect, authorize } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.get("/assignable", protect, authorize("admin"), getAssignableUsers);
+router.use(protect, authorize("admin"));
+router.get("/assignable", getAssignableUsers);
+router.get("/", getUsers);
+router.put("/:id", updateUser);
 
 module.exports = router;
